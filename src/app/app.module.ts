9a5fd684ace07d1448f0c79e7de60329731becf0
FazioNico <contact@nicolasfazio.ch>
2017-06-13 14:21:37 +0200

    clean code...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,15 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+// Import ngrx Tools
+import { NgRxStoreModule } from "../store/store.module";
+
+// Import App, Pages
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Import modules component
 import { NgzioGaugeComponentModule } from './ngzio-gauge/ngzio-gauge.module';
-
-// Import ngrx Tools
-import { NgRxStoreModule } from "../store/store.module";
 import { ControlsComponentModule } from '../components/controls/controls.module';
 
 @NgModule({
